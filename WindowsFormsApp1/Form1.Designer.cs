19772eed7055459ac8b15f996c56301d22550c22
Furkan-Can-Bayrak <furkancanb957@gmail.com>
2024-10-20 17:48:04 +0300

    Initial Commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pipeDown = new System.Windows.Forms.PictureBox();
+            this.bird = new System.Windows.Forms.PictureBox();
+            this.pipe = new System.Windows.Forms.PictureBox();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.scoreInput = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.recordInput = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pipeDown
+            // 
+            this.pipeDown.Image = ((System.Drawing.Image)(resources.GetObject("pipeDown.Image")));
+            this.pipeDown.Location = new System.Drawing.Point(491, -77);
+            this.pipeDown.Name = "pipeDown";
+            this.pipeDown.Size = new System.Drawing.Size(100, 291);
+            this.pipeDown.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeDown.TabIndex = 0;
+            this.pipeDown.TabStop = false;
+            // 
+            // bird
+            // 
+            this.bird.Image = ((System.Drawing.Image)(resources.GetObject("bird.Image")));
+            this.bird.Location = new System.Drawing.Point(106, 223);
+            this.bird.Name = "bird";
+            this.bird.Size = new System.Drawing.Size(69, 50);
+            this.bird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bird.TabIndex = 0;
+            this.bird.TabStop = false;
+            // 
+            // pipe
+            // 
+            this.pipe.Image = ((System.Drawing.Image)(resources.GetObject("pipe.Image")));
+            this.pipe.Location = new System.Drawing.Point(491, 396);
+            this.pipe.Name = "pipe";
+            this.pipe.Size = new System.Drawing.Size(100, 333);
+            this.pipe.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipe.TabIndex = 0;
+            this.pipe.TabStop = false;
+            // 
+            // ground
+            // 
+            this.ground.Image = ((System.Drawing.Image)(resources.GetObject("ground.Image")));
+            this.ground.Location = new System.Drawing.Point(0, 495);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(699, 114);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 0;
+            this.ground.TabStop = false;
+            this.ground.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // scoreInput
+            // 
+            this.scoreInput.AutoSize = true;
+            this.scoreInput.BackColor = System.Drawing.Color.White;
+            this.scoreInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.scoreInput.Location = new System.Drawing.Point(12, 530);
+            this.scoreInput.Name = "scoreInput";
+            this.scoreInput.Size = new System.Drawing.Size(115, 31);
+            this.scoreInput.TabIndex = 1;
+            this.scoreInput.Text = "Score:0";
+            this.scoreInput.Click += new System.EventHandler(this.Score_Click);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 20;
+            this.timer.Tick += new System.EventHandler(this.timerEvent);
+            // 
+            // recordInput
+            // 
+            this.recordInput.AutoSize = true;
+            this.recordInput.BackColor = System.Drawing.Color.White;
+            this.recordInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.recordInput.Location = new System.Drawing.Point(13, 9);
+            this.recordInput.Name = "recordInput";
+            this.recordInput.Size = new System.Drawing.Size(87, 25);
+            this.recordInput.TabIndex = 2;
+            this.recordInput.Text = "Rekor:0";
+            this.recordInput.Click += new System.EventHandler(this.record_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(696, 606);
+            this.Controls.Add(this.recordInput);
+            this.Controls.Add(this.scoreInput);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.pipe);
+            this.Controls.Add(this.bird);
+            this.Controls.Add(this.pipeDown);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pipeDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pipeDown;
+        private System.Windows.Forms.PictureBox bird;
+        private System.Windows.Forms.PictureBox pipe;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.Label scoreInput;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label recordInput;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int pipeSpeed = 15;   //Boruların hızı
+        int gravity = 10;       //yer çekimi
+        int score = 0;          //skor
+        Random rand = new Random(); //boruların konumları dinamik olması için random sınıfı
+        bool isJumping = false;     //zıplıyor mu değişkeni kuşun zıpladığını anlamak için
+        int record = 0;             //Rekor skoru tutar
+
+
+        public Form1()
+        {
+            InitializeComponent();  //Formu başlatır
+        }
+
+        private void Score_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timerEvent(object sender, EventArgs e)
+        {
+
+            if (isJumping)
+            {
+                // Zıplama hareketi için
+                bird.Top -= gravity; // Kuş yukarı hareket eder
+                isJumping = false; // Zıplama tamamlandı
+            }
+            else
+            {
+                // Yer çekimini uygula
+                bird.Top += gravity;
+            }
+
+
+            pipe.Left -= pipeSpeed;         //Alt boruyu hızı kadar sola hareket ettirir
+            pipeDown.Left -= pipeSpeed;     //Üst boruyu hızı kadar sola hareket ettirir
+            scoreInput.Text = "Score: " + score;        //Kişinin skorunu yazar
+
+            if (pipeDown.Left < -150)       //Eğer üst boru x koordinatında -150ye gelmişse
+            {
+                pipeDown.Left = 800;                // x=800 koordinatına götür
+                pipeDown.Top = rand.Next(-200, 0);  // y koordinatında -200 0 arasında random değer üret. Üretilen değer y koordinatı olacak
+            }
+
+            if (pipe.Left < -150)           //Eğer alt boru x koordinatında -150ye gelmişse
+            {
+                pipe.Left = 800;            // x=800 koordinatına götür
+                pipe.Top = pipeDown.Top + pipeDown.Height + rand.Next(145, 165);    // üst boruya göre arasında 145 165 arası mesafe olacak şekilde ayarlandı
+                score++;                //skoru 1 arttır
+            }
+
+            if (bird.Bounds.IntersectsWith(pipe.Bounds) ||          //Eğer boruya veya zemine değerse true döner
+                bird.Bounds.IntersectsWith(pipeDown.Bounds) ||
+                bird.Bounds.IntersectsWith(ground.Bounds)
+                )
+            {                   
+                if (score > record)         //skor rekordan büyükse artık yeni skor yeni rekordur
+                {
+                    record = score;
+                }
+                endGame();      //oyunu bitiren fonksiyon
+            }
+
+            if (bird.Top < 0)
+            {
+                bird.Top = 0;       //gözükmeyen yerlere uçmasın diye tavandan yukarı çıkması engelleniyor
+            }
+        }
+
+        private void keyDown(object sender, KeyEventArgs e)
+        {
+            // Oyun devam ediyorsa zıplamaya izin ver
+            if (e.KeyCode == Keys.Space && !isJumping && timer.Enabled)
+            {
+                isJumping = true; // Zıplama başladı
+                bird.Top -= 50; // Kuş yukarı hareket eder
+            }
+
+            // Oyun bittiğinde 'R' tuşuna basıldığında oyunu yeniden başlat
+            if (e.KeyCode == Keys.R && !timer.Enabled)
+            {
+                RestartGame(); // Oyunu yeniden başlat
+            }
+        }
+
+        private void keyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                isJumping = false; // Tuş bırakılınca zıplama durumu sıfırlanır 
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) //Oyun başladığında çalışan fonksiyon
+        {
+            timer.Interval = 30; // Timer interval ayarlanıyor (30 ms)
+            timer.Start();
+        }
+
+        private void endGame()
+        {
+            timer.Stop();
+            scoreInput.Text = "Oyun Bitti!! Skorun: " + score + "\nTekrar oynamak için 'R' tuşuna basın.";
+        }
+        private void RestartGame()
+        {
+            // Başlangıç değerlerini sıfırlayın
+            score = 0;
+            bird.Top = 100; // Kuşun başlangıç yüksekliği
+
+            // Boruları yeniden konumlandırın
+            pipe.Left = 800; // Boru başlangıç konumu
+            pipeDown.Left = 800; // Üst boru başlangıç konumu
+
+            timer.Start(); // Timer'ı başlat
+            scoreInput.Text = "Score: " + score; // Skoru sıfırlayın
+            recordInput.Text = "Rekor: " + record;
+        }
+
+        private void record_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
